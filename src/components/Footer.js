31bee8311e3c91678f810fbb7c1e9f905cc8825f
Muhil <110896154+muhil-1926@users.noreply.github.com>
2025-09-13 12:50:57 +0530

    Footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,13 +8,13 @@ import twitter from "../assets/twitter_icon.svg"
 import linkedin from "../assets/linkedin_icon.svg"
 import Modal from "./Modal";
 function Footer() {
-      
+
     // Modal State & Function
-        const [modalToggle, setModalToggle] = useState(false);
-        function toggle() {
-            setModalToggle(!modalToggle);
-        }
-        // -------------------
+    const [modalToggle, setModalToggle] = useState(false);
+    function toggle() {
+        setModalToggle(!modalToggle);
+    }
+    // -------------------
 
     return (
         <>
@@ -22,7 +22,7 @@ function Footer() {
             <div className="coming-soon">
                 <h1 className="font-atteron text-[32px] sm:text-[49px] font-normal leading-normal text-white w-[328px] text-center sm:w-full ">Our Products page will be live soon</h1>
                 <button className="font-poppins text-white text-[24px] font-extralight leading-normal bg-[#4B001A] rounded-[93px] py-[20px] px-[26px] cursor-pointer mt-7 sm:mt-20 " onClick={toggle}
->Get in Touch</button>
+                >Get in Touch</button>
             </div>
             <div className="footer-section ">
                 <div className="flex flex-col gap-y-20 sm:flex-row sm:items-center sm:justify-between px-3 pt-5 pb-12 sm:py-0 sm:pt-0 sm:px-[70px] ">
@@ -31,7 +31,7 @@ function Footer() {
 
                         <div className=" flex flex-row items-center gap-x-6 w-fit ml-10 sm:ml-14">
                             <a href="https://www.instagram.com" target="_blank"><img src={instagram} className="w-[30px] h-[31px]" /></a>
-                            <a href="https://www.linkedin.com" target="_blank"><img src={whatsapp} className="w-[40px] h-[40px]" /></a>
+                            <a href="https://www.whatsapp.com" target="_blank"><img src={whatsapp} className="w-[40px] h-[40px]" /></a>
                         </div>
                     </div>
 
@@ -46,9 +46,9 @@ function Footer() {
                 </div>
 
                 <Modal
-                modal={modalToggle}
-                active={toggle}
-            />
+                    modal={modalToggle}
+                    active={toggle}
+                />
 
             </div>
 
