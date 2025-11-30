211d22b55f406f649a8a18fd4218fec33160af7c
Algorism-prithiv <124758688+Algorism-prithiv@users.noreply.github.com>
2025-11-30 11:51:46 +0530

    Updates on home & Navbar

--- src/pages/Terms_Condition.js
+++ src/pages/Terms_Condition.js
@@ -141,6 +141,7 @@ function Terms_Condition() {
                         <p className="mt-3">Tarangi will acknowledge all complaints within 48 hours and endeavour to resolve them within the prescribed statutory timeframe.</p>
                     </div>
                     
+
                 </div>
             </div>
 
